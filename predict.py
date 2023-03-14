e85c2a9a6cb0eaedbd9b0845b59c8a8fb46834ed
matias <matiasahrensdorf@gmail.com>
2023-03-13 20:54:16 -0400

    maker sure file path is correct and data file is being recognized

--- predict.py
+++ predict.py
@@ -1,11 +1,12 @@
 from IPython.display import display
-
 import pandas as pd
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder ='data/MDatafiles_Stage0'
 
-Seeds = pd.read_csv(folder+"MNCAATourneySeeds.csv")
+folder ='MDataFiles_Stage1'
+
+Seeds = pd.read_csv(folder + '/' + 'MNCAATourneySeeds.csv')
 
 display(Seeds)
